e8e377fac4ba58447f9d483418ac307d36cd6739
sigh-q <sigh-q@qq.com>
2019-01-05 10:37:14 +0800

    首页完成

--- js/common.js
+++ js/common.js
@@ -12,4 +12,47 @@ $(document).ajaxStop(function(){
 
     },1000);
     
-})
+});
+
+
+
+$(function(){
+    
+    /**
+     * 点击二级菜单显示与隐藏
+     */
+    $(".lt-aside .category").click(function(){
+        $(this).next().stop().slideToggle();
+    })
+
+    /**
+     * 点击左边侧边栏的显示与隐藏
+     */
+
+     $(".lt-top .icon-left").click(function(){
+         $(".lt-aside").toggleClass("hidemenu");
+         $(".lt-top").toggleClass("hidemenu");
+         $(".lt-main").toggleClass("hidemenu");
+     });
+
+
+     /**
+      * 退出功能
+      */
+    $("#sure").click(function(){
+        $.ajax({
+            type:"get",
+            url:"/employee/employeeLogout",
+            dataType:"json",
+            success:function( info ){
+                console.log(info);
+                if( info.success){
+                    location.href = "login.html";
+                }
+            }
+        })
+    })
+
+});
+
+
